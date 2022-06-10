a08bd9b42011111e7f5371d9874395f7fb11b365
dqarias <dino.quispea@gmail.com>
2022-06-10 17:32:13 -0500

    Fix linter errors

--- __tests__/tasks.test.js
+++ __tests__/tasks.test.js
@@ -1,87 +1,86 @@
-const {stringLength, reverseString, Calculator, capitalize} = require('../src/index.js');
+const {
+  stringLength, reverseString, Calculator, capitalize,
+} = require('../src/index.js');
 
-test('comparing string length', ()=>{
-    const test = 'Ronald'
-    expect(stringLength(test)).toBe(6)
-})
+test('comparing string length', () => {
+  const test = 'Ronald';
+  expect(stringLength(test)).toBe(6);
+});
 
-test('string greater that ten characters',()=>{
-    const test = 'Procrastination'
-    expect(()=>stringLength(test)).toThrow(Error)
-})
+test('string greater that ten characters', () => {
+  const test = 'Procrastination';
+  expect(() => stringLength(test)).toThrow(Error);
+});
 
-test('Reversing a string', ()=>{
-    const test = 'Ronald'
-    expect(reverseString(test)).toBe('dlanoR')
-})
+test('Reversing a string', () => {
+  const test = 'Ronald';
+  expect(reverseString(test)).toBe('dlanoR');
+});
 
-test('Reversing a string', ()=>{
-    const test = 'Friend'
-    expect(reverseString(test)).toMatch(/dneirF/)
-})
+test('Reversing a string', () => {
+  const test = 'Friend';
+  expect(reverseString(test)).toMatch(/dneirF/);
+});
 
 describe('Adding numbers tests', () => {
-    test('10 plus 5 returns 15', () => {
-        expect(Calculator.add(10,5)).toBe(15);
-    });
-    test('5 plus 7 returns 15', () => {
-        expect(Calculator.add(5,7)).toBe(12);
-    });
-    test('5 plus -7 returns -2', () => {
-        expect(Calculator.add(5,-7)).toBe(-2);
-    });
-  
+  test('10 plus 5 returns 15', () => {
+    expect(Calculator.add(10, 5)).toBe(15);
   });
+  test('5 plus 7 returns 15', () => {
+    expect(Calculator.add(5, 7)).toBe(12);
+  });
+  test('5 plus -7 returns -2', () => {
+    expect(Calculator.add(5, -7)).toBe(-2);
+  });
+});
 
-  describe('Substracting number test',() => {
-    test('10 minus 5 returns 5', () => {
-        expect(Calculator.sustract(10,5)).toBe(5)
-    })
-    test('10 minus -5 returns 15', () => {
-        expect(Calculator.sustract(10,-5)).toBe(15)
-    })
-    test('40 minus 5 returns 35', () => {
-        expect(Calculator.sustract(40,5)).toBe(35)
-    })
-  })
+describe('Substracting number test', () => {
+  test('10 minus 5 returns 5', () => {
+    expect(Calculator.sustract(10, 5)).toBe(5);
+  });
+  test('10 minus -5 returns 15', () => {
+    expect(Calculator.sustract(10, -5)).toBe(15);
+  });
+  test('40 minus 5 returns 35', () => {
+    expect(Calculator.sustract(40, 5)).toBe(35);
+  });
+});
 
-  describe('Multipyng number test',() => {
-    test('10 times 5 returns 50', () => {
-        expect(Calculator.multiply(10,5)).toBe(50)
-    })
-    test('10 times -5 returns -50', () => {
-        expect(Calculator.multiply(10,-5)).toBe(-50)
-    })
-    test('10 times 0 returns 0', () => {
-        expect(Calculator.multiply(10,0)).toBe(0)
-    })
-    
-  })
+describe('Multipyng number test', () => {
+  test('10 times 5 returns 50', () => {
+    expect(Calculator.multiply(10, 5)).toBe(50);
+  });
+  test('10 times -5 returns -50', () => {
+    expect(Calculator.multiply(10, -5)).toBe(-50);
+  });
+  test('10 times 0 returns 0', () => {
+    expect(Calculator.multiply(10, 0)).toBe(0);
+  });
+});
 
-  describe('Dividing number test',() => {
-    test('10 divide 5 returns 2', () => {
-        expect(Calculator.divide(10,5)).toBe(2)
-    })
-    test('60 divide 5 returns 12', () => {
-        expect(Calculator.divide(60,5)).toBe(12)
-    })
-    test('60 divide 0 returns Error', () => {
-        expect(()=>Calculator.divide(60,0)).toThrow(Error)
-    })
-    
-  })
+describe('Dividing number test', () => {
+  test('10 divide 5 returns 2', () => {
+    expect(Calculator.divide(10, 5)).toBe(2);
+  });
+  test('60 divide 5 returns 12', () => {
+    expect(Calculator.divide(60, 5)).toBe(12);
+  });
+  test('60 divide 0 returns Error', () => {
+    expect(() => Calculator.divide(60, 0)).toThrow(Error);
+  });
+});
 
-    describe('Testing Capitalize string',() => {
-        test('First testing', () => {
-            const string = 'Ronald'
-            expect(capitalize(string)).toBe('Ronald')
-        })
-        test('second testing', () => {
-            const string = 'peru'
-            expect(capitalize(string)).toBe('Peru')
-        })
-        test('second testing', () => {
-            const string = 'paises'
-            expect(capitalize(string)).toBe('Paises')
-        })
-    })
+describe('Testing Capitalize string', () => {
+  test('First testing', () => {
+    const string = 'Ronald';
+    expect(capitalize(string)).toBe('Ronald');
+  });
+  test('second testing', () => {
+    const string = 'peru';
+    expect(capitalize(string)).toBe('Peru');
+  });
+  test('second testing', () => {
+    const string = 'paises';
+    expect(capitalize(string)).toBe('Paises');
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,67 +1,69 @@
-/************************ Task 1 ************************/
-// Write a function stringLength(string) that takes any string as an argument and returns its characters count.
+/** ********************** Task 1 *********************** */
+// Write a function stringLength(string) that takes any
+// string as an argument and returns its characters count.
 
 const stringLength = (string) => {
- let countCharacter = string.length;
- if( countCharacter >= 1 && countCharacter <= 10){
+  const countCharacter = string.length;
+  if (countCharacter >= 1 && countCharacter <= 10) {
     return countCharacter;
- }else{
-    throw new Error('Your string length is less equal than 1 or greater equal than 10')
- }
-}
+  }
+  throw new Error('Your string length is less equal than 1 or greater equal than 10');
+};
 
-/************************ Task 2 ************************/
-//Write a function reverseString(string) function. It should take a string as an argument and return it reversed.
+/** ********************** Task 2 *********************** */
+// Write a function reverseString(string) function.
+// It should take a string as an argument and return it reversed.
 
-const reverseString = (string) =>{
-    let array = string.split('')
-    let newArray = [];
-    let newString = '';
-    for(i=array.length-1; i>=0 ;i--){
-        newArray.push(array[i])
-    }
-    newString = newArray.join('');
-    return newString;
-}
+const reverseString = (string) => {
+  const array = string.split('');
+  const newArray = [];
+  let newString = '';
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    newArray.push(array[i]);
+  }
+  newString = newArray.join('');
+  return newString;
+};
 
-/************************ Task 3 ************************/
-//Write a simple calculator class or object, which will have 4 methods: add, subtract, divide, and multiply.
+/** ********************** Task 3 *********************** */
+// Write a simple calculator class or object, which will
+// have 4 methods: add, subtract, divide, and multiply.
 
 class Calculator {
-   
-    static add(numbera,numberb){
-        const resultAdd = numbera + numberb;
-        return resultAdd;
-    }
+  static add(numbera, numberb) {
+    const resultAdd = numbera + numberb;
+    return resultAdd;
+  }
 
-    static sustract(numbera,numberb){
-        const resultSustract = numbera - numberb;
-        return resultSustract;
-    }
+  static sustract(numbera, numberb) {
+    const resultSustract = numbera - numberb;
+    return resultSustract;
+  }
 
-    static multiply(numbera,numberb){
-        const resultMultiply = numbera * numberb;
-        return resultMultiply;
-    }
+  static multiply(numbera, numberb) {
+    const resultMultiply = numbera * numberb;
+    return resultMultiply;
+  }
 
-    static divide(numbera,numberb){
-        const resultDivide = numbera / numberb;
-        if(numberb===0){
-         throw new Error('can not divide a number with 0');
-        }else{
-         return resultDivide ;
-        }
+  static divide(numbera, numberb) {
+    const resultDivide = numbera / numberb;
+    if (numberb === 0) {
+      throw new Error('can not divide a number with 0');
+    } else {
+      return resultDivide;
     }
+  }
 }
 
-/************************ Task 4 ************************/
-//capitalize(string) function.
+/** ********************** Task 4 *********************** */
+// capitalize(string) function.
 const capitalize = (string) => {
-    let stringCapitalize = string.slice(0, 1).toUpperCase();
-    let stringLowerCase = string.slice(1)
-    let result = stringCapitalize.concat(stringLowerCase)
-    return result;
+  const stringCapitalize = string.slice(0, 1).toUpperCase();
+  const stringLowerCase = string.slice(1);
+  const result = stringCapitalize.concat(stringLowerCase);
+  return result;
+};
 
-}
-
-module.exports = {stringLength, reverseString, Calculator, capitalize} ; 
+module.exports = {
+  stringLength, reverseString, Calculator, capitalize,
+};
